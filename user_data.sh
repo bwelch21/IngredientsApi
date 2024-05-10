f88ea0969b484b4e49185b26d961252249b25a21
Brandt Welch <bwelch@hopper.com>
2024-05-10 17:27:07 -0400

    cleanup

--- user_data.sh
+++ user_data.sh
@@ -9,11 +9,9 @@ aws_region="us-east-1"
 aws_account_number="***REMOVED***"
 image_name="$aws_account_number.dkr.ecr.$aws_region.amazonaws.com/ingredients-api"
 
-
 aws ecr get-login-password --region "$aws_region" | docker login --username AWS --password-stdin "$aws_account_number".dkr.ecr."$aws_region".amazonaws.com
 
-# docker pull $image_name:latest
-
 docker run -dit --rm -w /ingredients-api -p 8000:8000 \
-  -e OPENAI_API_KEY=***REMOVED*** \
+  -e OPENAI_API_KEY=$OPENAI_API_KEY \
+  -e ALLERGY_INSIGHTS_KEY=$ALLERGY_INSIGHTS_KEY> \
   $image_name
